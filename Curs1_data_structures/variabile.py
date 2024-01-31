4d0eea998f3a0c17e40accf84b1381c5f7a03e70
Mihai Ilie <mihai.ilie@nxdata.ro>
2024-01-31 21:40:10 +0200

    variabile

---
+++ Curs1_data_structures/variabile.py
@@ -0,0 +1,15 @@
+# Venitul saptamanal al unui salariat
+# venit_pe_ora / cate_ore/saptamanal
+
+venit_pe_ora = 12
+cate_ore = 40
+venit_saptamanal = venit_pe_ora * cate_ore
+print(venit_saptamanal)
+venit_saptamanal = venit_saptamanal + venit_saptamanal
+print(venit_saptamanal)
+
+1name = 10 # asa nu
+name 1 = 10 # asa nu
+VPO = 10 # merge dar nu recomand
+venitPeOra = 10 # merge dar nu recomand
+def, pass, from, import, del, and, in, is, if, else, return # asa nu
