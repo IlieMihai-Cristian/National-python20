22614841d865d1041aac0da3dca50c438e91df59
Mihai Ilie <mihai.ilie@nxdata.ro>
2024-05-08 12:33:33 +0300

    django3

--- Curs8_django/proiect/aplicatie1/views.py
+++ Curs8_django/proiect/aplicatie1/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from django.urls import reverse
 from aplicatie1.models import Location
+from django.shortcuts import redirect
 
 
 # CreateView -> adaugarea datelor noi in baza de date (instante noi)
@@ -25,3 +26,31 @@ class CreateLocationsView(CreateView):
         return reverse('locations:lista_locatii')
 
 
+class UpdateLocationsView(UpdateView):
+    model = Location
+    fields = ['city', 'country']
+    template_name = 'aplicatie1/locations_form.html'
+
+    def get_success_url(self):
+        return reverse('locations:lista_locatii')
+
+
+def delete_location(request, pk):
+    # SQL nativ: "SELECT id, city FROM aplicatie1_location WHERE id=1"
+    # Location.objects.get(id=1).city
+
+    # 1.cu get:
+    # instance_object = Location.objects.get(country='Romania')
+    # instance_object = Location.objects.get(id=pk)
+    # if instance_object.active is True:
+    #     instance_object.active = False
+    #     instance_object.save()
+
+    # 2.cu filter
+    Location.objects.filter(id=pk).update(active=False)
+    return redirect('locations:lista_locatii')
+
+
+def activate_location(request, pk):
+    Location.objects.filter(id=pk).update(active=True)
+    return redirect('locations:lista_locatii')
--- Curs8_django/proiect/proiect/settings.py
+++ Curs8_django/proiect/proiect/settings.py
@@ -124,3 +124,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR), 'static')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/locations/'
+
+LOGOUT_REDIRECT_URL = '/'
